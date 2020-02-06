51b42f34a765f931a7084df7a59275e7a358149d
Intedont <56801389+Intedont@users.noreply.github.com>
2020-02-06 20:57:55 +0300

    Теперь местоположение меняется динмически, добавил проверку gps

--- app/src/main/java/com/example/superpuper_ar_rpg/MapsActivity.java
+++ app/src/main/java/com/example/superpuper_ar_rpg/MapsActivity.java
@@ -1,15 +1,21 @@
 package com.example.superpuper_ar_rpg;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
+
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.WindowManager;
 import android.widget.Toast;
@@ -22,79 +28,104 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
 
-import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationClient;
-    private int locationRequestCode = 1000;
+//    private int locationRequestCode = 1000;
+    private LocationManager mLocationManager;
     private double wayLatitude =  43, wayLongitude = 169;
+    private SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
         setContentView(R.layout.activity_maps);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE); //привязываем locationManager к сервису контроля за gps???
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        //КУСОК НЕПОНЯТНОГО ГОВНА НАЧАЛО
-        //Проверяем permission на местоположение
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
-                    locationRequestCode);
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        //Проверяем, включен ли gps
+        if(!mLocationManager.isProviderEnabled("gps")){
+            GPSEnableDialog gpsEnable = new GPSEnableDialog();
+            gpsEnable.show(getSupportFragmentManager(), "what`s the tag?");
+        }
+
+        //проверям, разрешено ли использовать gps (надо дописать)
+        if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+        }
+
+        //Обновляем координаты вызывая mlocationListener через callback
+        mLocationManager.requestLocationUpdates(mLocationManager.GPS_PROVIDER, 10000, 1, mlocationListener);
+    }
+
+    //создаем окно с просьбой включить gps
+    public static class GPSEnableDialog extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.dialog_GPSEnableMessage);
+            builder.setPositiveButton(R.string.dialog_GPSEnableAgree, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //пиздуй в настройки включать геолокацию
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            builder.setNegativeButton(R.string.dialog_GPSEnableRefuse, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //ну как хочешь
+                    dialogInterface.cancel();
+                }
+            });
+            return builder.create();
+        }
+    }
+
+    //реализуем интерфейс LocationListener
+    LocationListener mlocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            //при обновлении локации обновляем карту
+            Toast.makeText(MapsActivity.this, "LocationChanged", Toast.LENGTH_SHORT).show();
+            mapFragment.getMapAsync(MapsActivity.this);
+            Log.d("LocationChanged", "Longtitude " + location.getLongitude() + " Latitude " + location.getLatitude());
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
 
         }
-        //else {
-            // already permission granted
-//            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
-//                if (location != null) {
-//                    wayLatitude = location.getLatitude();
-//                    wayLongitude = location.getLongitude();
-//                    Toast toast = Toast.makeText(getApplicationContext(),
-//                            wayLatitude + " " + wayLongitude, Toast.LENGTH_LONG);
-//                    toast.show();
-//                    Log.d("TAG", "Latitude " + wayLatitude + " Longtitude " + wayLongitude);
-//                }
-//            });
-        //}
-        //КУСОК НЕПОНЯТНОГО ГОВНА КОНЕЦ
-    }
 
-    //КУСОК НЕПОНЯТНОГО ГОВНА НАЧАЛО
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        switch (requestCode) {
-//            case 1000: {
-//                // If request is cancelled, the result arrays are empty.
-//                if (grantResults.length > 0
-//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    mFusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
-//                        if (location != null) {
-//                            wayLatitude = location.getLatitude();
-//                            wayLongitude = location.getLongitude();
-//                            Log.d("TAG1", "Latitude " + wayLatitude + " Longtitude " + wayLongitude);
-//                        }
-//                    });
-//                } else {
-//                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
-//                }
-//                break;
-//            }
-//        }
-    }
-    //КУСОК НЕПОНЯТНОГО ГОВНА КОНЕЦ
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+
 
     /**
      * Manipulates the map once available.
@@ -108,17 +139,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+
         mFusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
                 if (location != null) {
-//                    wayLatitude = location.getLatitude();
-//                    wayLongitude = location.getLongitude();
-
                     LatLng myCoordinates = new LatLng(location.getLatitude(), location.getLongitude());
                     Log.d("TAG2", "Latitude " + wayLatitude + " Longtitude " + wayLongitude);
 
+//                  mMap.clear();
                     mMap.addMarker(new MarkerOptions().position(myCoordinates).title("Marker in ... what is it?"));
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(myCoordinates));
                     Toast.makeText(MapsActivity.this, "Ha ha, found you, Lenny", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MapsActivity.this, "Location == null", Toast.LENGTH_SHORT).show();
+                    Log.e("TAG3", "Latitude " + wayLatitude + " Longtitude " + wayLongitude);
                 }
         });
     }
