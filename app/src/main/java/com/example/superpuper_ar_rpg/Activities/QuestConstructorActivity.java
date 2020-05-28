24ec4b466fe9e8540ce3a21ba3bcf76b2f304398
Intedont <muraddusov@bk.ru>
2020-05-28 15:46:54 +0300

    Почти работает

---
+++ app/src/main/java/com/example/superpuper_ar_rpg/Activities/QuestConstructorActivity.java
@@ -0,0 +1,14 @@
+package com.example.superpuper_ar_rpg.Activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuestConstructorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
